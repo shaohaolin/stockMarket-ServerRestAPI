2094c7dfaf43af333c7eefb496c8afc55ebaf418
Shaohao Lin <slin232@uwo.ca>
2015-04-01 18:46:36 -0400

    make the code clean

--- server.js
+++ server.js
@@ -192,63 +192,6 @@ app.get('/companies/:company_id', function(request, response) {
     });
 });
 
-/*
-var Posts = mongoose.model('Posts', postsSchema);
-
-
-app.get('/posts', function (request, response) {
-    Posts.find(function (error, posts) {
-        if (error) response.send(error);
-        response.json({posts: posts});
-    });
-});
-
-app.get('/posts/:post_id', function (request, response) {
-    Posts.findById(request.params.post_id, function (error, post) {
-        if (error) response.send(error);
-        response.json({posts: post});
-    });
-});
-
-app.post('/posts', function (request, response) {
-    var post = new Posts({
-        title: request.body.post.title,
-        body: request.body.post.body
-    });
-    post.save(function(error) {
-        if (error) response.send(error);
-        response.status(201).json({posts: post});
-    });
-});
-
-app.put('/posts/:post_id', function (request, response) {
-    // use our Posts model to find the post we want
-    Posts.findById(request.params.post_id, function (error, post) {
-        if (error) response.send(error);
-
-        // update the post info
-        post.title = request.body.post.title,
-        post.body = request.body.post.body
-
-        // save the post
-        post.save(function (error) {
-            if (error) response.send(error);
-            response.status(201).json({posts: post});
-        });
-    });
-});
-
-app.delete('/posts/:post_id', function (request, response) {
-    Posts.remove({
-        _id: request.params.post_id
-    }, function(error, post) {
-        if (error) response.send(err);
-        response.status(201).json({posts: Posts});
-    });
-
-});
-*/
-
 app.listen(3700, function () {
     console.log('Listening on port 3700');
 });
